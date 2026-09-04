376f0f231232be7151e3943d2ac25a16651d4fa2
rajapandi29 <rp6321613@gmail.com>
2026-09-04 12:55:01 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,14 +1,14 @@
 module "vpc" {
   source = "git::https://github.com/Rajapandi29/terraform-modules.git//vpc?ref=v1.0.0"
 
-  name = var.project_name
-  cidr = var.vpc_cidr
+  name = var.name
+  cidr = var.cidr
 
   azs = [
     "ap-south-1a",
     "ap-south-1b"
   ]
 
-  public_subnets  = var.public_subnet_cidrs
-  private_subnets = var.private_subnet_cidrs
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
