4f575c4d7a1132a3d3bc08bd5870ea0fbf9cb80a
rajapandi29 <rp6321613@gmail.com>
2026-09-04 12:18:38 +0530

    new

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "terraform-state-04-09"
     key          = "buckets/terraform-state"
-    region       = "as-south-1"
+    region       = "ap-south-1"
     encrypt      = true
     use_lockfile = true
   }
