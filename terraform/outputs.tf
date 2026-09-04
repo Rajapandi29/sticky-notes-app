2d9f16ced753314c1d4cd82f06e9579381882d13
rajapandi29 <rp6321613@gmail.com>
2026-09-04 12:43:54 +0530

    new

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,10 +1,14 @@
 output "vpc_id" {
-  value = module.vpc.vpc_id
+  description = "The ID of the VPC"
+  value       = module.vpc.vpc_id
 }
 
 output "public_subnet_ids" {
-  value = module.vpc.public_subnet_cidrs
+  description = "Public subnet IDs"
+  value       = module.vpc.public_subnet_ids
 }
+
 output "private_subnet_ids" {
-  value = module.vpc.private_subnet_ids
+  description = "Private subnet IDs"
+  value       = module.vpc.private_subnet_ids
 }
