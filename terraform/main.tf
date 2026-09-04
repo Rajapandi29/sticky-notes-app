c442e3f20dc4f0e6d3820e3d15c4952f318e00dd
rajapandi29 <rp6321613@gmail.com>
2026-09-04 12:14:22 +0530

    new

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,8 +1,8 @@
 module "vpc" {
       source = "git::https://github.com/Rajapandi29/terraform-modules.git//vpc?ref=v1.0.0"
 
-  project_name         = var.project_name
-  vpc_cidr             = var.vpc_cidr
-  public_subnet_cidrs  = var.public_subnet_cidrs
-  private_subnet_cidrs = var.private_subnet_cidrs
+  name            = var.name
+  cidr            = var.cidr
+  public_subnets  = var.public_subnets
+  private_subnets = var.private_subnets
 }
--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,20 +1,20 @@
-variable "project_name" {
-    type = map(string)
+variable "name" {
+    type = string
 
 }
 variable "aws_region" {
      type = string
      default = "ap-south-1"
 }
-variable "vpc_cidr" {
+variable "cidr" {
     type = string
     default = "10.0.0.0/16"
 }
-variable "public_subnet_cidrs" {
+variable "public_subnets" {
     type = list(string)
     default = ["10.0.0.0/24", "10.0.11.0/24"]
 }
-variable "private_subnet_cidrs" {
+variable "private_subnets" {
     type = list(string)
     default = ["10.0.1.0/24", "10.0.12.0/24"]
 }
